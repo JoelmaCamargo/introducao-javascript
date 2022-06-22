5f44ac5e775e36bfd957101bc7cee17406583121
Joelma Camargo <joelmavilela023@gmail.com>
2022-06-22 17:03:14 -0300

    Declarando variáveis no escopo do arquivo e da função

--- intro-js/index.js
+++ intro-js/index.js
@@ -1,18 +1,6 @@
-document.querySelector('#t1').textContent = 'JavaScript - Introdução';
-document.querySelector('#t2').textContent = 'Exemplificando algumas funcionalidades';
+//ESCOPO DE VARIÁVEIS E CONSTANTES
 
-//apresenta uma mensagem e esconde o form de formularioLogin
-
-/*
-function bemVindo(){
-    const nome = document.querySelector('#nomeUsuario').value;
-    alert(`Olá ${nome}! Seja bem-vindo(a)!`);
-    document.getElementById('formularioLogin').style.display = 'none';
-}
-*/
-
-function mudaCorFundo(){
-   const menu = document.querySelector('#coresFundo');
-   const cor = menu.options[menu.selectedIndex].value;
-   document.body.style.background = cor;
+function calculaKmMilha(){
+    let km = document.querySelector('#valorKm').value;
+    document.querySelector('#resultado').textContent = km * 1.609;
 }
